d01c96cfcaa6acfc8ce302aa7aa6b1b94891b97e
Pavan Kalyan Neerukonda <49464941+pavan-98@users.noreply.github.com>
2025-06-21 11:49:33 +0000

    added default quote load

--- js/quotes.js
+++ js/quotes.js
@@ -143,9 +143,8 @@ function openURL(url) {
     window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 
-getQuote();
-
 document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
     const newQuoteButton = document.getElementById('newquote');
     if (newQuoteButton) {
         newQuoteButton.addEventListener('click', getQuote);
